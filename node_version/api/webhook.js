f727e23ff751d9f805ace8976040f5d2171b1d69
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-01-04 16:06:14 -0700

    vercel serverless set up

---
+++ node_version/api/webhook.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto');
+const { db } = require('../db');
+
+const verify = ({ signingKey, timestamp, token, signature }) => {
+    const encodedToken = crypto
+        .createHmac('sha256', signingKey)
+        .update(timestamp.concat(token))
+        .digest('hex');
+
+    return encodedToken === signature;
+};
+
+module.exports = (req, res) => {
+    cors()(req, res, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        if (req.method === 'POST') {
+            const { signingKey, timestamp, token, signature, sender } =
+                req.body;
+            if (!verify({ signingKey, timestamp, token, signature })) {
+                return res.status(403).send('Invalid signature');
+            }
+            const emails = db
+                .collection('clients')
+                .doc('uid')
+                .collection('emails');
+            emails
+                .where('response_received', '==', sender)
+                .get()
+                .then((snapshot) => {
+                    snapshot.forEach((doc) => {
+                        doc.ref.update({ response_received: true });
+                    });
+                });
+            res.send('ok');
+        } else {
+            res.status(405).send('Access Forbidden');
+        }
+    });
+};
