f9c9f2c83a9ef8dc6464eb763b8e758f020d587d
Shaun Offenbacher <shaunoffenbacher@yahoo.com>
2024-01-06 00:15:59 -0700

    bug hunting

--- node_version/db.js
+++ node_version/db.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
+console.log('firebaseConfig', firebaseConfig);
+
 admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig),
 });
