c99f1821a61f5c4de0969d1036342610ac0f4ef4
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-01-15 12:34:21 -0700

    Dynamic webhooks

--- node_version/api/webhook.js
+++ node_version/api/webhook.js
@@ -13,15 +13,41 @@ export default async (req, res) => {
     try {
         if (req.method === 'POST') {
             console.log(req.body);
-            const { signingKey, timestamp, token, signature, sender } =
-                req.body;
+            const {
+                signingKey,
+                timestamp,
+                token,
+                signature,
+                sender,
+                recipient,
+            } = req.body;
             if (!verify({ signingKey, timestamp, token, signature })) {
                 return res.status(403).send('Invalid signature');
             }
+
+            // Extract the domain from the recipient email
+            const domain = recipient.split('@')[1];
+
+            // Search the client collection for a document with the extracted domain
+            const clientDoc = await db
+                .collection('clients')
+                .where('domain', '==', domain)
+                .get();
+
+            // If no client is found, return an error
+            if (clientDoc.empty) {
+                return res.status(404).send('Client not found');
+            }
+
+            // Get the uid of the client
+            const uid = clientDoc.docs[0].id;
+
+            // Use the uid to grab the email collection
             const emails = db
                 .collection('clients')
-                .doc('uid')
+                .doc(uid)
                 .collection('emails');
+
             const snapshot = await emails
                 .where('response_received', '==', sender)
                 .get();
