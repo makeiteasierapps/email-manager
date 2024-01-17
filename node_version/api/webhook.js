b3e335ff739d071872cc64e181afc977590d7073
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-01-17 10:08:37 -0700

    Adding Ai respons

--- node_version/api/webhook.js
+++ node_version/api/webhook.js
@@ -1,5 +1,6 @@
 import { createHmac } from 'crypto';
 import { db } from '../db.js';
+import { aiEmailResponse } from '../services/aiService.js';
 
 const verify = ({ timestamp, token, signature }) => {
     const signingKey = process.env.MAILGUN_WEBHOOK_KEY;
@@ -14,6 +15,7 @@ export default async (req, res) => {
     try {
         if (req.method === 'POST') {
             const { timestamp, token, signature, sender, recipient } = req.body;
+            console.log(req.body);
 
             if (!verify({ timestamp, token, signature })) {
                 return res.status(403).send('Invalid signature');
@@ -32,10 +34,12 @@ export default async (req, res) => {
             if (clientDoc.empty) {
                 return res.status(404).send('Client not found');
             }
-            console.log('clientDoc', clientDoc.docs[0].id);
 
             // Get the uid of the client
             const uid = clientDoc.docs[0].id;
+
+            // Client name
+            const clientName = clientDoc.docs[0].data().to_name;
 
             // Use the uid to grab the email collection
             const emails = db
@@ -43,9 +47,7 @@ export default async (req, res) => {
                 .doc(uid)
                 .collection('emails');
 
-            const snapshot = await emails
-                .where('to_email', '==', sender)
-                .get();
+            const snapshot = await emails.where('to_email', '==', sender).get();
             snapshot.forEach((doc) => {
                 doc.ref.update({ response_received: true });
             });
